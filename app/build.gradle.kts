0837c7233b0b9250f04bfd4e416ba9652e1f6eba
pelemen1054 <149672916+pelemen1054@users.noreply.github.com>
2026-09-17 20:33:37 +1000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         versionName = "1.0.0"
     }
 
+    compileOptions {
+    sourceCompatibility JavaVersion.VERSION_17
+    targetCompatibility JavaVersion.VERSION_17
+}
     buildFeatures {
         compose = true
     }
